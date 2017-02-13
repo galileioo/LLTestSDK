21f5a63aa96a8b8996717266f57815b89a231b80
galileioo <galileio@126.com>
2017-02-13 13:43:09 +0800

    Initial Commit

---
+++ LLTestSDK/LLTestSDK.h
@@ -0,0 +1,13 @@
+//
+//  LLTestSDK.h
+//  LLTestSDK
+//
+//  Created by galileio on 2017/2/13.
+//  Copyright © 2017年 galileioo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LLTestSDK : NSObject
+
+@end
---
+++ LLTestSDK/LLTestSDK.m
@@ -0,0 +1,13 @@
+//
+//  LLTestSDK.m
+//  LLTestSDK
+//
+//  Created by galileio on 2017/2/13.
+//  Copyright © 2017年 galileioo. All rights reserved.
+//
+
+#import "LLTestSDK.h"
+
+@implementation LLTestSDK
+
+@end
